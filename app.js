d4ea6ce9b2ea7e3db61f66ac5319fb8a06922ebc
Ayoub Ebn El Hadj <ayoubbelhadj1998@gmail.com>
2024-12-26 22:23:40 +0100

    test 1

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = 3000;
 
+// Middleware pour analyser le JSON dans les requêtes
+app.use(express.json());
+
+// Route principale
 app.get('/', (req, res) => {
-  res.json({ message: 'Bonjour depuis le pipeline CI/CD!' });
+    res.send('Hello hello Aymen and Rami');
 });
 
-app.listen(port, () => {
-  console.log(`Serveur démarré sur le port ${port}`);
+// Route pour gérer le webhook GitHub
+app.post('/github-webhook', (req, res) => {
+    console.log('Headers:', req.headers);
+    console.log('Body:', req.body);
+    res.status(200).send('Webhook traité avec succès');
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
 });
